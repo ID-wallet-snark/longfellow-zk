7cd8f01128d7484e5ed545f0c037dbf5f74e338c
FirPic <firpic@mailo.eu>
2026-01-03 22:58:43 +0100

    Modification du type de namespace. AJout d'attribut dans le namespace

--- lib/circuits/mdoc/mdoc_attribute_ids.h
+++ lib/circuits/mdoc/mdoc_attribute_ids.h
@@ -33,17 +33,17 @@ constexpr const char* kISO23220PhotoIDNamespace =
     "org.iso.23220.photoID.1";
 constexpr const char* kISO23220DTCNamespace = "org.iso.23220.dtc.1";
 constexpr const char* kFRANTSMDL1Namespace = "fr.gouv.ants.mdl.1";
-constexpr const char* kFRStudentMDL1Namespace = "fr.gouv.education.mdl.1";
+constexpr const char* kFRStudent1Namespace = "fr.gouv.education.1";
 
 constexpr const char* kSupportedNamespaces[] = {
     kMDLNamespace,        kAAMVANamespace,    kEUAVNamespace,
     kEUDIPIDNamespace,    kISO23220Namespace, kISO23220PhotoIDNamespace,
-    kISO23220DTCNamespace, kFRANTSMDL1Namespace, kFRStudentMDL1Namespace};
+    kISO23220DTCNamespace, kFRANTSMDL1Namespace, kFRStudent1Namespace};
 
 constexpr const char* kIDPassDocType = "com.google.wallet.idcard.1";
 constexpr const char* kMDLDocType = "org.iso.18013.5.1.mDL";
 constexpr const char* kFRDocType = "fr.gouv.ants.mdl.1.permis";
-constexpr const char* kFRStudentDocType = "fr.gouv.education.mdl.1.student";
+constexpr const char* kFRStudentDocType = "fr.gouv.education.1.student";
 constexpr const char* kEUAVDocType = "eu.europa.ec.av.1";
 constexpr const char* kEUDIPIDDocType = "eu.europa.ec.eudi.pid.1";
 constexpr const char* kISO23220PhotoIDDocType = "org.iso.23220.photoID.1";
@@ -290,7 +290,10 @@ constexpr MdocAttribute kMdocAttributes[] = {
     {"category_DE_expiry", kFRANTSMDL1Namespace},
 
     // French Student
-    {"is_student", kFRStudentMDL1Namespace},
+    {"family_name", kFRStudent1Namespace},
+    {"given_name", kFRStudent1Namespace},
+    {"birth_date", kFRStudent1Namespace},
+    {"is_student", kFRStudent1Namespace},
 };
 
 }  // namespace proofs
